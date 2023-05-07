d0085e7d453de44d765b88f18e9ce3bc56f2f01f
W-Novytskyi <novickijvaceslav011@gmail.com>
2023-05-07 19:33:41 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, Mod } from './Modal.styled';
 import PropTypes from 'prop-types';
@@ -6,22 +6,26 @@ import PropTypes from 'prop-types';
 const modalRoot = document.querySelector('#modal-root');
 
 export default function Modal(props) {
+  const handleKeyDown = useCallback(
+    event => {
+      if (event.code === 'Escape') {
+        props.onClose();
+      }
+    },
+    [props]
+  );
+
   const handleOverlayClick = event => {
     if (event.currentTarget === event.target) props.onClose();
   };
 
   useEffect(() => {
-    const handleKeyDown = event => {
-      if (event.code === 'Escape') {
-        props.onClose();
-      }
-    };
     window.addEventListener('keydown', handleKeyDown);
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [props]);
+  }, [handleKeyDown]);
 
   return createPortal(
     <Overlay onClick={handleOverlayClick}>
